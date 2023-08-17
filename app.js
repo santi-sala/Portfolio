2fe4214d69ef649b30f0ab7d806b537d12ca5e17
Santiago Sala <pipo0716@gmail.com>
2023-08-17 09:48:02 +0300

    Burger toggling

--- app.js
+++ app.js
@@ -79,8 +79,9 @@ function animateSlides() {
   });
 }
 
-let mouse = document.querySelector(".cursor");
-let mouseText = mouse.querySelector("span");
+const mouse = document.querySelector(".cursor");
+const mouseText = mouse.querySelector("span");
+const burger = document.querySelector(".burger");
 
 /**********CURSOR ANIMATION *********/
 //Following cursor
@@ -112,6 +113,31 @@ function activeCursor(e) {
 }
 /**********CURSOR ANIMATION *********/
 
+/********** BURGER ANIMATION *********/
+function navToggle(e) {
+  if (!e.target.classList.contains("active")) {
+    e.target.classList.add("active");
+    gsap.to(".line1", 0.5, { rotate: "45", y: 5, background: "black" });
+    gsap.to(".line2", 0.5, { rotate: "-45", y: -5, background: "black" });
+    gsap.to("#logo", 1, { color: "black" });
+    gsap.to(".footer-bar", 1, { clipPath: "circle(2500px at 100% -10%)" });
+    document.body.classList.add("hide");
+  } else {
+    e.target.classList.remove("active");
+    gsap.to(".line1", 0.5, { rotate: "0", y: 0, background: "white" });
+    gsap.to(".line2", 0.5, { rotate: "-0", y: 0, background: "white" });
+    gsap.to("#logo", 1, { color: "white" });
+    gsap.to(".footer-bar", 1, { clipPath: "circle(50px at 100% -10%)" });
+    document.body.classList.remove("hide");
+  }
+}
+/********** BBURGER ANIMATION *********/
+
+/********** EVENT LISTENERS **********/
+burger.addEventListener("click", navToggle);
 window.addEventListener("mousemove", cursor);
 window.addEventListener("mouseover", activeCursor);
+
+/********** EVENT LISTENERS **********/
+
 animateSlides();
